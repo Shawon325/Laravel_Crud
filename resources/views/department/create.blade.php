d2ce47208b30aef97b5126a21d778e4ed50784b7
Bardock325 <mynameisshawon325@gmail.com>
2020-02-18 01:39:16 +0600

    mastering

--- resources/views/department/create.blade.php
+++ resources/views/department/create.blade.php
@@ -1,4 +1,40 @@
-<!DOCTYPE html>
+@extends('layout.index')
+
+@section('content')
+<div class="card">
+                            <form method="post" action="{{route('department.store')}}">
+                              @csrf
+                                <div class="card-body">
+                                    <h4 class="card-title">Department Info</h4>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 text-right control-label col-form-label">Department Name</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="Enter Department Name" name="department_name">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Description</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="lname" placeholder="Enter Description" name="description">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email1" class="col-sm-3 text-right control-label col-form-label">Status</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="email1" placeholder="Enter Status" name="status">
+                                        </div>
+                                    </div>
+                                <div class="border-top">
+                                    <div class="card-body">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+@endsection
+
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Bootstrap Example</title>
@@ -41,4 +77,4 @@
 </div>
 
 </body>
-</html>
+</html> -->
--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -1,4 +1,44 @@
-<!DOCTYPE html>
+@extends('layout.index')
+
+@section('content')
+<div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Department Datatable</h5>
+                                  <a href="{{route('department.create')}}"><button class="btn btn-success">Add New</button></a>
+                                  <br>
+                                  <br>
+                                <div class="table-responsive">
+                                    <table id="zero_config" class="table table-striped table-bordered">
+                                      
+                                        <thead>
+                                          <tr>
+                                            <th>Sl</th>
+                                            <th>Department Name</th>
+                                            <th>Description</th>
+                                            <th>Status</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          @php $sl=1;  @endphp
+                                          @foreach($data as $info)
+                                          <tr>
+                                            <td>{{$sl++}}</td>
+                                            <td>{{$info->department_name}}</td>
+                                            <td>{{$info->description}}</td>
+                                            <td>{{$info->status}}</td>
+                                          </tr>
+                                          @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+@endsection
+
+
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Teacher Information</title>
@@ -40,3 +80,4 @@
 </html>
 
 
+ -->
